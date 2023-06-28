0ca6084f0c78f6f34faaba1f77cf0ea7b161beca
SkyBestPL <szymon.gwozdziej@gmail.com>
2023-06-29 01:41:50 +0200

    turnieje kontynuacja

--- ProjektTurnieju/ProjektTurnieju/Pages/Organizator/CreateTurniej.cshtml.cs
+++ ProjektTurnieju/ProjektTurnieju/Pages/Organizator/CreateTurniej.cshtml.cs
@@ -13,21 +13,49 @@ namespace ProjektTurnieju.Pages.Organizator
 {
     public class CreateTurniejModel : PageModel
     {
+		private readonly TurniejDBContext _context;
+
+		public CreateTurniejModel(TurniejDBContext context)
+		{
+			_context = context;
+		}
+
 		[BindProperty]
 		public Turniej newTurniej { get; set; }
 
+		public List<Druzyna> DruzynyList { get; set; }
+
 		public void OnGet()
 		{
+			PopulateDruzynyList();
 		}
 		public IActionResult OnPost()
 		{
 			if (ModelState.IsValid == false)
+			{
+				PopulateDruzynyList();
 				return Page();
+			}
 			
 			DBTurniej database = new DBTurniej();
 			database.Dodaj(newTurniej);
 
 			return RedirectToPage("/Organizator/ListaTurnieji");
+		}
+
+		private void PopulateDruzynyList()
+		{
+			var druzyny = _context.Druzyny;
+			DruzynyList = new List<Druzyna>();
+
+			foreach (var druzyna in druzyny)
+			{
+				DruzynyList.Add(new Druzyna
+				{
+					Id = druzyna.Id,
+					NazwaDruzyny = druzyna.NazwaDruzyny
+				});
+			}
 		}
 	}
 }
