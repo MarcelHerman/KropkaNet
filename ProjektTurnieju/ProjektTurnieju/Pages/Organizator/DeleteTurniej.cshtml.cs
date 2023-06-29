f2e7bff6bf3c6cde50ed7dd37bf45be182b31f07
SkyBestPL <szymon.gwozdziej@gmail.com>
2023-06-29 13:38:05 +0200

    usuwanie turnieju poprawione

--- ProjektTurnieju/ProjektTurnieju/Pages/Organizator/DeleteTurniej.cshtml.cs
+++ ProjektTurnieju/ProjektTurnieju/Pages/Organizator/DeleteTurniej.cshtml.cs
@@ -1,30 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ProjektTurnieju.Data;
+using ProjektTurnieju.Migrations;
 using ProjektTurnieju.Models;
-using ProjektTurnieju.DBActions;
+
 
 namespace ProjektTurnieju.Pages.Organizator
 {
     public class DeleteTurniejModel : PageModel
     {
-        [BindProperty(SupportsGet = true)]
-        public int Id { get; set; }
-        public Turniej turniej = new Turniej();
-        public void OnGet()
+        private readonly ProjektTurnieju.Data.TurniejDBContext _context;
+
+        public DeleteTurniejModel(ProjektTurnieju.Data.TurniejDBContext context)
         {
-            DBTurniej database = new DBTurniej();
-            turniej = database.getOne(Id);
+            _context = context;
         }
 
-        public IActionResult OnPost()
-        {
-            if (Id == 1)
-            {
+        [BindProperty]
+        public Turniej Turniej { get; set; } = default!;
 
-                return RedirectToPage("/Blad");
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Turniej == null)
+            {
+                return NotFound();
             }
-            DBTurniej database = new DBTurniej();
-            database.Usun(turniej, Id);
+
+            var turniej = await _context.Turniej.Include(Turniej => Turniej.Druzyny).FirstOrDefaultAsync(m => m.Id == id);
+
+            if (turniej == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Turniej = turniej;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Turniej == null)
+            {
+                return NotFound();
+            }
+            var turniej = await _context.Turniej.Include(Turniej => Turniej.Druzyny).FirstOrDefaultAsync(m => m.Id == id);
+
+            if (turniej != null)
+            {
+                Turniej = turniej;
+
+                foreach (var druzyna in Turniej.Druzyny)
+                {
+                    Turniej.Druzyny.Remove(druzyna);
+                }
+
+                _context.Turniej.Remove(Turniej);
+                await _context.SaveChangesAsync();
+            }
+
             return RedirectToPage("/Organizator/ListaTurnieji");
         }
     }
